041f660bc5594270682928561c5398c7e80dc49e
ssm8887 <ssm8887@naver.com>
2021-01-11 12:53:13 +0900

    UnitySource 추가

---
+++ UnitySource/UnityCamTest3d/Assets/RotateObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float xSpeed = 1;
+    public float ySpeed = 1;
+    public float zSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.Rotate(xSpeed, ySpeed, zSpeed);
+    }
+}
